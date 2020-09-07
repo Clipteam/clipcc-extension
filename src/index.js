972d489fdb0dde3d7be422b357ef41f45fd14b1a
Alex Cui <cuizhihui030925@outlook.com>
2020-09-08 02:27:55 +0800

    modified argument api

--- src/index.js
+++ src/index.js
@@ -10,32 +10,52 @@ class SampleExtension extends extension.Extension {
     }
     init() {
         this.addCategory({
-            categoryId: 'sample',
-            messageId: 'clipcc.json.sample',
+            categoryId: 'test',
+            messageId: 'Test',
             color: '#66CCFF'
         });
         this.addBlock({
-            opcode: 'get',
-            type: extension.BlockType.COMMAND,
-            messageId: 'clipcc.json.block.get',
-            categoryId: 'clipcc.sample.sample',
-            func: args => 233
-        });
-        this.addCategory({
-            categoryId: 'test',
-            messageId: 'gui.extension.clipcc.json.description',
-            color: '#FFB11B'
+            opcode: 'test1',
+            type: extension.BlockType.BOOLEAN,
+            messageId: 'TEST1 [ARG1] [ARG2]',
+            categoryId: 'clipcc.sample.test',
+            function: args => args.ARG1 === args.ARG2,
+            argument: {
+                ARG1: {
+                    type: extension.ArgumentType.STRING,
+                    default: 'aaa'
+                },
+                ARG2: {
+                    type: extension.ArgumentType.STRING,
+                    default: 'bbb'
+                }
+            }
         });
         this.addBlock({
-            opcode: 'testests',
+            opcode: 'test2',
             type: extension.BlockType.REPORTER,
-            messageId: 'TEST [ARG]',
+            messageId: 'TEST2 [ARG] [BOOL]',
             categoryId: 'clipcc.sample.test',
-            func: args => args.ARG + 1,
-            arguments: {
+            function: args => args.BOOL ? args.ARG : args.ARG + 2,
+            argument: {
                 ARG: {
                     type: extension.ArgumentType.NUMBER,
-                    default: '1'
+                    default: 1
+                },
+                BOOL: {
+                    type: extension.ArgumentType.BOOLEAN
+                }
+            }
+        });
+        this.addBlock({
+            opcode: 'test3',
+            type: extension.BlockType.COMMAND,
+            messageId: 'TEST3 [COLOR]',
+            categoryId: 'clipcc.sample.test',
+            function: args => args.COLOR,
+            argument: {
+                COLOR: {
+                    type: extension.ArgumentType.COLOR
                 }
             }
         });
