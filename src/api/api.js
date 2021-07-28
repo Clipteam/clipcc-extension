48e343e8e007ce5f8cec7119fba365f60d1f7ced
Alex Cui <cuizhihui030925@outlook.com>
2021-07-28 23:24:39 +0800

    Implement callGlobalFunction & registerGlobalFunction.

--- src/api/api.js
+++ src/api/api.js
@@ -4,66 +4,7 @@
  */
 
 let hasInit = false;
-let apiInstance = null;
-
-/**
- * Add a category from proptype.
- * @param {Category} categoryInfo - Category proptype. 
- */
-const addCategory = categoryInfo => apiInstance.vm.addCategory(categoryInfo);
-
-/**
- * Remove a category from id.
- * @param {string} categoryId - Category id.
- */
-const removeCategory = categoryId => apiInstance.vm.removeCategory(categoryId);
-
-/**
- * Add a block from proptype.
- * @param {Block} blockInfo - Block proptype.
- */
-const addBlock = blockInfo => apiInstance.vm.addBlock(blockInfo);
-
-/**
- * Remove a block from opcode.
- * @param {string} blockOpcode - Block opcode.
- */
-const removeBlock = blockOpcode => apiInstance.vm.removeBlock(blockOpcode);
-
-/**
-*  Get data for playground.
-*/
-const getPlaygroundData = () => apiInstance.vm.getPlaygroundData();
-
-/**
-* Load a Scratch project from a .sb, .sb2, .sb3 or json string.
-* @param {string | object} input A json string, object, or ArrayBuffer representing the project to load.
-* @param {function} extensionCallback A function to deal with extension list.
-* @return {!Promise} Promise that resolves after targets are installed.
-*/
-const loadProject = (input, extensionCallback) => apiInstance.vm.loadProject(input, extensionCallback);
-
-/**
-* Return whether the editor is in the desktop environment.
-*/
-const isDesktop = () => apiInstance.gui.isDesktop();
-
-/**
-* Return whether the editor is loading.
-*/
-const isEditorLoading = () => apiInstance.gui.isEditorLoading();
-
-const migrateChangeBlock = (targets, srcBlockId, dstBlockId) => {
-    for (const target of targets) {
-        console.log(target.blocks._blocks);
-        for (const blockId in target.blocks._blocks) {
-            console.log(blockId);
-            if (target.blocks._blocks[blockId].opcode === srcBlockId) {
-                target.blocks._blocks[blockId].opcode = dstBlockId;
-            }
-        }
-    }
-};
+let instance = null;
 
 /**
 * Get the GUI.
@@ -90,22 +31,69 @@ let vm = null;
 */
 function registExtensionAPI(api) {
     if (hasInit) return;
-    apiInstance = api;
-    this.gui = apiInstance.gui.gui;
-    this.vm = apiInstance.vm.vm;
-    this.blockly = apiInstance.blocks;
+    instance = api;
+    this.gui = instance.gui.gui;
+    this.vm = instance.vm.vm;
+    this.blockly = instance.blocks;
     hasInit = true;
 }
 
+/**
+ * Add a category from proptype.
+ * @param {Category} categoryInfo - Category proptype. 
+ */
+ const addCategory = categoryInfo => instance.vm.addCategory(categoryInfo);
+
+ /**
+  * Remove a category from id.
+  * @param {string} categoryId - Category id.
+  */
+ const removeCategory = categoryId => instance.vm.removeCategory(categoryId);
+ 
+ /**
+  * Add a block from proptype.
+  * @param {Block} blockInfo - Block proptype.
+  */
+ const addBlock = blockInfo => instance.vm.addBlock(blockInfo);
+ 
+ /**
+  * Remove a block from opcode.
+  * @param {string} blockOpcode - Block opcode.
+  */
+ const removeBlock = blockOpcode => instance.vm.removeBlock(blockOpcode);
+ 
+ /**
+  *  Get data for playground.
+  */
+ const getPlaygroundData = () => instance.vm.getPlaygroundData();
+ 
+ /**
+  * Load a Scratch project from a .sb, .sb2, .sb3 or json string.
+  * @param {string | object} input A json string, object, or ArrayBuffer representing the project to load.
+  * @param {function} extensionCallback A function to deal with extension list.
+  * @return {!Promise} Promise that resolves after targets are installed.
+  */
+ const loadProject = (input, extensionCallback) => instance.vm.loadProject(input, extensionCallback);
+ 
+ /**
+  * Return whether the editor is in the desktop environment.
+  */
+ const isDesktop = () => instance.gui.isDesktop();
+ 
+ /**
+  * Return whether the editor is loading.
+  */
+ const isEditorLoading = () => instance.gui.isEditorLoading();
+
 module.exports = {
+    vm,
+    blockly,
+    registExtensionAPI,
+    ...require('./util'),
     addCategory,
     removeCategory,
     addBlock,
     removeBlock,
     getPlaygroundData,
-    loadProject,
-    migrateChangeBlock,
-    vm,
-    blockly,
-    registExtensionAPI
+    loadProject
 };
---
+++ src/api/util.js
@@ -0,0 +1,46 @@
+// Utils
+
+const globalFuncion = {};
+
+/**
+ * Register a global function.
+ * @param {string} name - Function name.
+ * @param {Function} func - Function instance.
+ */
+const registerGlobalFunction = (name, func) => {
+    globalFuncion[name] = func;
+};
+
+/**
+ * Call a registered global function.
+ * @param {string} name - Function name.
+ * @param {...any} args - Arguments.
+ * @returns {any} - Result
+ */
+const callGlobalFunction = (name, ...args) => {
+    return globalFuncion[name](...args);
+};
+
+/**
+ * Change a block to another.
+ * @param {any} targets - Scratch project JSON.
+ * @param {string} srcBlockId - Source block ID.
+ * @param {string} dstBlockId - Destination block ID.
+ */
+const migrateChangeBlock = (targets, srcBlockId, dstBlockId) => {
+    for (const target of targets) {
+        console.log(target.blocks._blocks);
+        for (const blockId in target.blocks._blocks) {
+            console.log(blockId);
+            if (target.blocks._blocks[blockId].opcode === srcBlockId) {
+                target.blocks._blocks[blockId].opcode = dstBlockId;
+            }
+        }
+    }
+};
+
+module.exports = {
+    registerGlobalFunction,
+    callGlobalFunction,
+    migrateChangeBlock
+};
